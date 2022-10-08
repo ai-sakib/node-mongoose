6f07416269cf55810436517ab0dc250a0bcda07c
Asiful Islam Sakib <sakib2439@gmail.com>
2022-10-08 23:15:13 +0600

    feat: user added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const errorController = require('./controllers/error')
+const User = require('./models/user')
 
 const app = express()
 const mongoose = require('mongoose')
@@ -17,14 +18,14 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use((req, res, next) => {
-//     User.find('6341684e1447690e689a2f92')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user._id, user.cart)
-//             next()
-//         })
-//         .catch(err => console.log(err))
-// })
+app.use((req, res, next) => {
+    User.findById('6341af09ed3fea3514d28734')
+        .then(user => {
+            req.user = user
+            next()
+        })
+        .catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
@@ -36,6 +37,20 @@ mongoose
     .connect(
         'mongodb+srv://mern_stack:OHYq2jFfxE91wYg2@cluster0.yqodw.mongodb.net/shop?retryWrites=true&w=majority'
     )
+    .then(result => {
+        return User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Sakib',
+                    email: 'sakib@test.com',
+                    cart: {
+                        items: [],
+                    },
+                })
+                user.save()
+            }
+        })
+    })
     .then(result => {
         app.listen(port, () =>
             console.log(`Example app listening on port ${port}!`)
